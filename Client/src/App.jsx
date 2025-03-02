c56afc794a7b2c253f929899456095888078df2f
Blissysurya <150290655+Blissysurya@users.noreply.github.com>
2025-03-03 00:15:55 +0530

    as

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -3,19 +3,23 @@ import './App.css'
 
 import { GoogleLogin } from '@react-oauth/google'
 import { jwtDecode } from "jwt-decode";
-
+import Header from './components/Header';
 function App() {
 
   return (
-    <GoogleLogin
-    onSuccess={credentialResponse => {
-      const decoded = jwtDecode(credentialResponse.credential);
-      console.log(decoded);
-    }}
-    onError={() => {
-      console.log('Login Failed');
-    }}
-  />)
+  //   <GoogleLogin
+  //   onSuccess={credentialResponse => {
+  //     const decoded = jwtDecode(credentialResponse.credential);
+  //     console.log(decoded);
+  //   }}
+  //   onError={() => {
+  //     console.log('Login Failed');
+  //   }}
+  // />
+  <>
+  <Header/>
+  </>
+  )
   
 }
 
---
+++ Client/src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+export default function Header() {
+  return (
+    <div>
+        <h1 className="ml-[10px] font-bold text-white shadow-lg text-4xl">
+          PlayGround
+        </h1>
+    </div>
+    
+  )
+}
